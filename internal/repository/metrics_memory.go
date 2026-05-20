186e42381bcd685776f3eb4d94c5d1de128ea668
Mikhail Pashkov <workpashkovmikhail@gmail.com>
2026-05-20 21:43:38 +0300

    iter11: server: MetricsMemoryRepository - rename receiver

---
+++ internal/repository/metrics_memory.go
@@ -0,0 +1,89 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"maps"
+	"slices"
+	"sync"
+
+	"github.com/mikhailpashkov/metrics/internal/model"
+)
+
+type MetricsMemoryRepository struct {
+	storage         map[int64]*models.Metrics
+	lastId          int64
+	mu              sync.RWMutex
+	errNotFound     error
+	errMetricsIsNil error
+}
+
+func NewMetricsMemoryRepository() *MetricsMemoryRepository {
+	return &MetricsMemoryRepository{
+		storage:         make(map[int64]*models.Metrics),
+		lastId:          -1,
+		errNotFound:     errors.New("not found"),
+		errMetricsIsNil: errors.New("metrics is nil"),
+	}
+}
+
+func (r *MetricsMemoryRepository) FindById(ctx context.Context, id int64) (*models.Metrics, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	metrics, ok := r.storage[id]
+	if !ok {
+		return nil, r.errNotFound
+	}
+	return metrics, nil
+}
+
+func (r *MetricsMemoryRepository) FindByName(ctx context.Context, name string) ([]*models.Metrics, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	metrics := make([]*models.Metrics, 0)
+	for _, metric := range r.storage {
+		if metric.Name == name {
+			metrics = append(metrics, metric)
+		}
+	}
+	return metrics, nil
+}
+
+func (r *MetricsMemoryRepository) FindAll(ctx context.Context) ([]*models.Metrics, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return slices.Collect(maps.Values(r.storage)), nil
+}
+
+func (r *MetricsMemoryRepository) Save(ctx context.Context, metrics *models.Metrics) (*models.Metrics, error) {
+	if metrics == nil {
+		return nil, r.errMetricsIsNil
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if metrics.ID == -1 {
+		r.lastId++
+		metrics.ID = r.lastId
+	}
+	r.storage[metrics.ID] = metrics
+	return metrics, nil
+}
+
+func (r *MetricsMemoryRepository) DeleteAll(ctx context.Context) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.storage = make(map[int64]*models.Metrics)
+	r.lastId = -1
+	return nil
+}
+
+func (r *MetricsMemoryRepository) DeleteById(ctx context.Context, id int64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.storage, id)
+	return nil
+}
