f8ae1822ef7bfd4010384b6bcb6bb9c03cd34e45
Mikhail Pashkov <workpashkovmikhail@gmail.com>
2026-05-27 14:05:55 +0300

    iter13: review: fix init migration: TEXT -> VARCHAR and index for metrics name

--- db/migrations/20260520111212_init.sql
+++ db/migrations/20260520111212_init.sql
@@ -2,12 +2,15 @@
 CREATE TABLE metrics
 (
     id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    ts    TIMESTAMP NOT NULL,
-    type  TEXT      NOT NULL,
-    name  TEXT      NOT NULL,
+    ts    TIMESTAMP    NOT NULL,
+    type  VARCHAR(64)  NOT NULL,
+    name  VARCHAR(256) NOT NULL,
     delta BIGINT,
     value DOUBLE PRECISION
 );
 
+CREATE INDEX idx_metrics_name ON metrics (name);
+
 -- +goose Down
 DROP TABLE metrics
+DROP INDEX IF EXISTS idx_metrics_name;
